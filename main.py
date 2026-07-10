fcd69ec0190e7c40c7d3636eb4eefc8f981ebee2
traceyomphile <omphiletracey@gmail.com>
2026-07-10 22:54:31 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,73 @@
+import torch as t
+from sklearn.datasets import load_iris
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
+import matplotlib.pyplot as plt
+
+def get_iris_data():
+    # Download and load the Iris data
+    iris = load_iris()
+
+    # Get features and labels
+    X_numpy, y_numpy, class_names = iris.data, iris.target, iris.target_names
+
+    # Convert to PyTorch Tensors
+    X = t.tensor(X_numpy, dtype=t.float32)
+    y = t.tensor(y_numpy, dtype=t.long)
+
+    return X, y, class_names
+
+
+def scale_data(X, y, train_ratio: float = 0.8):
+    # Split the data
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, 
+        y, 
+        train_size=train_ratio,
+        random_state=42,
+        shuffle=True
+    )
+
+    # Initialise the scaler
+    scaler = StandardScaler()
+
+    # Fit and transform the training data
+    scaled_train = scaler.fit_transform(X_train)
+    scaled_test = scaler.transform(X_test)
+
+    return scaled_train, y_train, scaled_test, y_test
+
+def iris_model(X_train, y_train, k: int = 5):
+    model = KNeighborsClassifier(n_neighbors=k)
+
+    trained_model = model.fit(X_train, y_train)
+
+    return trained_model
+
+def plot_results(y_true, y_pred, class_names):
+    cm = confusion_matrix(y_true, y_pred)
+
+    # Create and configure the visual plot
+    disp = ConfusionMatrixDisplay(cm, display_labels=class_names)
+    disp.plot(cmap=plt.cm.Blues)
+
+    plt.show()
+
+def main():
+    # Get Iris data and its class names
+    X, y, classes = get_iris_data()
+
+    # Get scaled and split data
+    X_train, y_train, X_test, y_test = scale_data(X, y)
+
+    # Train model for Iris Dataset
+    model = iris_model(X_train, y_train)
+    y_pred = model.predict(X_test)
+
+    # PLot Iris Results
+    plot_results(y_test, y_pred, classes)
+
+if __name__ == '__main__':
+    main()
